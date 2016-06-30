ea55c78522ba06737965e2ec57fcdcd7b8ff314d
AlbertDavid9199 <albert.david.9199@gmail.com>
2016-06-30 18:15:11 +0200

    recovery

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-var cors = require('cors'); // add this line below it
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 

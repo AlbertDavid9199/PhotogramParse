d3c4108fd019512c577b41a315db0fe6bfc72829
AlbertDavid9199 <albert.david.9199@gmail.com>
2016-06-30 21:02:13 +0200

    again

--- cloud/main.js
+++ cloud/main.js
@@ -12,7 +12,7 @@ var _ = require('underscore');
 var config = require('./config.js');
 require('./linkedin.js');
 // require('./migrations.js');
-// require('./jobs.js');
+require('./jobs.js');
 require('./app.js');
 require('./admin.js');
 require('./video.js');

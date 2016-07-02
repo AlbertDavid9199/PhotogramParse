049ca6efa7888a986439332c5a1b7264b6dbf553
AlbertDavid9199 <albert.david.9199@gmail.com>
2016-07-02 16:49:21 +0200

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
-
+require('./mongo-indexes.js');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {

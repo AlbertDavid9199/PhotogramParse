b52c906570fbeb3c87441e7dffe7c6bf0c2c17e1
AlbertDavid9199 <albert.david.9199@gmail.com>
2016-07-27 18:04:52 +0200

    update again

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey

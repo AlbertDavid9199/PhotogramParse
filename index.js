f03427d71d011024de5ea4a1646fedc7d2342cda
NyamEnkhbat <nyam.enkhbat.1029@gmail.com>
2016-07-27 18:01:57 +0200

    update

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'PandaID',
-  masterKey: process.env.MASTER_KEY || 'PandaMasterKey', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://pandasphereapp.herokuapp.com/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'Panda_ID',
+  masterKey: process.env.MASTER_KEY || 'Panda_Master_Key', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://pandaparse.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
